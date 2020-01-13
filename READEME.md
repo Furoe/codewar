5fdcf930d11d9af5e7c5e6f226e8b29f80aae699
yangwenqing <furo_yang@163.com>
2020-01-13 17:57:17 +0800

    solve credit card mask

---
+++ READEME.md
@@ -0,0 +1,3 @@
+##### Credit Card Mask
+* solution: if cc.length <= 4, return cc;
+			else backup the last 4 chars, replace cc with '#', then set the last 4 chars.
---
+++ creditCardMask.js
@@ -0,0 +1,16 @@
+// return masked string
+function maskify(cc) {
+    let len = cc.length;
+    let temp = cc.split('');
+    if(len > 4){
+        cc = cc.replace(/\w|\d/g, '#');
+        var dd = cc.split('');
+        dd[len-1] = temp[len-1];
+        dd[len-2] = temp[len-2];
+        dd[len-3] = temp[len-3];
+        dd[len-4] = temp[len-4];
+        return dd.join('');
+    }else{
+      return cc;
+    }
+}
